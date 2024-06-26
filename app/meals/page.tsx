9110c08125c7b5d3fecf503b0cdde25be68faa89
JeonB <newjeonb@gmail.com>
2024-06-27 00:10:03 +0900

    sqlite 환경 추가

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const MealsPage = () => {
   return (
@@ -7,17 +8,15 @@ const MealsPage = () => {
       <header className={classes.header}>
         <h1>
           맛있는 음식들, created{" "}
-          <span className={classes.highligh}> by you </span>
-          <p>
-            제일 좋아하는 요리를 골라 스스로 요리해보세요. 쉽고 재밌답니다!{" "}
-          </p>
-          <p className={classes.cta}>
-            <Link href="/meals/share">최애 레시피 공유</Link>
-          </p>
+          <span className={classes.highlight}> by you </span>
         </h1>
+        <p>제일 좋아하는 요리를 골라 스스로 요리해보세요. 쉽고 재밌답니다! </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">최애 레시피 공유</Link>
+        </p>
       </header>
       <main className={classes.main}>
-        <h1>Meals</h1>
+        <MealsGrid meals={[]} />
       </main>
     </>
   );
