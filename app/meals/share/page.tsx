6b65961b9609a7b5c1ebf906beca317404c39a93
JeonB <newjeonb@gmail.com>
2024-07-16 18:25:35 +0900

    42. form 제출을 위한 서버 액션 사용법

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -2,16 +2,30 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  const shareMeal = async (formData: { get: (arg0: string) => any }) => {
+    "use server"; // server action 오직 서버에서만 사용하게 보장하는 키워드
+
+    const meal = {
+      creator_email: formData.get("email"),
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+    };
+    console.log(meal);
+  };
+
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Share your <span className={classes.highlight}>favorite meal</span>
+          ´ Share your <span className={classes.highlight}>favorite meal</span>
         </h1>
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +53,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="Image" name="image" />
+          <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
